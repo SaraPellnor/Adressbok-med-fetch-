d45d442b88462adc2a1f744fdb33177013c13b69
Sara Pellnor <sara.ahlquist@hotmail.com>
2022-11-15 11:02:13 +0100

    fetch file

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const adressBook = document.querySelector("adressBook")
+const book = document.querySelector("book")
+
+
+
+async function getFile(file) {
+  let objects = await fetch(file);
+  let list = await objects.json();
+  visibleAteam(list)
+}
+
+getFile("ateam.json");
+
+function visibleAteam(list) {
+    const adressList = list
+    adressList.forEach(object => {
+        console.log(object)
+    });
+}
+
+//console.log(list)
